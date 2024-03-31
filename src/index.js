9d1483e253641b5b550d0a51080bbb11ee017fca
DwashKarma <dwaskc@gmail.com>
2024-03-31 17:48:09 +0545

    refactor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ function Import() {
   let img = document.createElement("img");
   img.src = require("../assets/WFTlogo.jpg");
   let getElement = document.getElementById("logo");
+  getElement.innerHTML = "";
   getElement.appendChild(img);
 }
 Import();
