0e2127b99925c25fd0a79ef53391331a9d4d5ca9
DwashKarma <dwaskc@gmail.com>
2024-04-03 13:26:58 +0545

    add url in image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 (function () {
   let img = document.createElement("img");
-  img.src = "https://cdn.jsdelivr.net/npm/cdn-package@1.0.15/dist/WFTlogo.svg";
+  img.src =
+    "https://firebasestorage.googleapis.com/v0/b/wft-footer.appspot.com/o/WFTlogo.jpg?alt=media&token=d453e725-e4f6-42ad-9db8-f01b662ea0eb";
   let getElement = document.getElementById("logo");
   getElement.innerHTML = "";
   getElement.appendChild(img);
