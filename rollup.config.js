551ca62fd1007aaa5ade7f6fadb034ea89d6ebee
DwashKarma <dwaskc@gmail.com>
2024-04-03 10:14:28 +0545

    added image in public

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,27 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
-import babel from 'rollup-plugin-babel';
+import babel from "rollup-plugin-babel";
 import image from "@rollup/plugin-image";
 import copy from "rollup-plugin-copy";
 
-export default[
-    {
-        input:"src/index.js",
-        output:{
-            file:"dist/bundle.js",
-            format:"esm",
-            
-        },
-        plugins:[
-            nodeResolve(),
-            commonjs(),
-            image(),
-            babel({
-                exclude:'node_modules/**',
-                presets:['@babel/preset-env']
-            }),
-            copy({
-                targets:[
-                    {src:'assets/WFTlogo.jpg',dest:'dist/assets'}
-                ]
-            })
-        ]
-    }
-]
+export default [
+  {
+    input: "src/index.js",
+    output: {
+      file: "dist/bundle.js",
+      format: "esm",
+    },
+    plugins: [
+      nodeResolve(),
+      commonjs(),
+      image(),
+      babel({
+        exclude: "node_modules/**",
+        presets: ["@babel/preset-env"],
+      }),
+      copy({
+        targets: [{ src: "public/WFTlogo.jpg", dest: "dist/assets" }],
+      }),
+    ],
+  },
+];
