ec513099dfbfea043078ff3d4aded142842b4c8b
DwashKarma <dwaskc@gmail.com>
2024-04-03 12:47:56 +0545

    no change

--- rollup.config.js
+++ rollup.config.js
@@ -9,7 +9,8 @@ export default [
     input: "src/index.js",
     output: {
       file: "dist/bundle.js",
-      format: "esm",
+      format: "cjs",
+      sourcemap: true,
     },
     plugins: [
       nodeResolve(),
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 (function () {
   let img = document.createElement("img");
-  img.src = "/WFTlogo.svg";
+  img.src = "./WFTlogo.svg";
   let getElement = document.getElementById("logo");
   getElement.innerHTML = "";
   getElement.appendChild(img);
